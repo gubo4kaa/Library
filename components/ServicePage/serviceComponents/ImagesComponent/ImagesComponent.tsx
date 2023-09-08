dcd7ef14982623c89105aecb8669a7eded993752
Danila <dany.den2014@yandex.ru>
2023-09-08 19:14:17 +0300

    debug image public .env

--- components/ServicePage/serviceComponents/ImagesComponent/ImagesComponent.tsx
+++ components/ServicePage/serviceComponents/ImagesComponent/ImagesComponent.tsx
@@ -10,7 +10,7 @@ interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,HTMLDiv
 export default function ImagesComponent({service}:Props) {
   return (
     <div className={styles.wrapper}>  
-        <Image  src={`${process.env.LOCAL_LIBRARY_API}/${service.id}/${service.images[1]}`} width={1142} height={736} alt='' className={styles.image}/>
+        <Image priority={true}  src={`${process.env.NEXT_PUBLIC_LIBRARY_API}/${service.id}/${service.images[1]}`} width={1142} height={736} alt='' className={styles.image}/>
         {/* <img src={`${process.env.NEXT_PUBLIC_LIBRARY_API}/${service.id}/${service.images[0]}`} alt='' className={styles.image}/> */}
     </div>
   )
