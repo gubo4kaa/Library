71fcb1acfe5b8bcfa600893c81a7135a5f609e1d
Danila <dany.den2014@yandex.ru>
2023-09-12 17:24:44 +0300

    debug form fetch

--- components/SubscribeForm/SubscribeForm.tsx
+++ components/SubscribeForm/SubscribeForm.tsx
@@ -30,7 +30,6 @@ export default function SubscribeForm({}:Props) {
     let redirectUrl = 'https://uiscore.io'
     const onSubmit = async (data: any) => {
         setLoadingState(true)
-      
         try {
           let response = await fetch(formUrl, {
             method: 'POST',
@@ -79,7 +78,7 @@ export default function SubscribeForm({}:Props) {
                 loadingState && !accessState && <>LOADING</>
             }
             {   !loadingState && !accessState &&
-                <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+                <form action="https://my-store.lemonsqueezy.com/email-subscribe/external" method="post" className={styles.form}>
                     <svg className={styles.searchLogo} width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g id="Inbox">
                         <path id="Square" fillRule="evenodd" clipRule="evenodd" d="M2.29102 11C2.29102 4.58329 4.12435 3.66663 10.9998 3.66663C17.8752 3.66663 19.7077 4.58329 19.7077 11C19.7077 17.4166 17.8752 18.3333 10.9998 18.3333C4.12435 18.3333 2.29102 17.4166 2.29102 11Z" stroke="#6E7A90" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
@@ -118,6 +117,9 @@ export default function SubscribeForm({}:Props) {
                         </ButtonNew>
                     </span>
                 </form>
+            }
+            {
+                accessState && <p>Отправлено</p>
             }
             <p>
                 No spam, only updates and releases. 
