ae0054b3c705292ee8eaaebfda58c743f77646f8
Danila <dany.den2014@yandex.ru>
2023-10-01 13:32:29 +0300

    debug documents ym

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,21 +10,24 @@ export default function Document() {
         <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-      
-                ym(95109351, "init", {
+                  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              
+                  ym(95109351, "init", {
                       clickmap:true,
                       trackLinks:true,
-                      accurateTrackBounce:true
-                });
+                      accurateTrackBounce:true,
+                      webvisor:true
+                  });
               `,
             }}
           />
         <noscript>
           <div>
-            <img src="https://mc.yandex.ru/watch/95109351" style={{ position:'absolute', left:'-9999px' }} alt="" />
+            <img src="https://mc.yandex.ru/watch/95109351" style={{ position:'absolute', left:'-9999px' }} alt=""/>
           </div>
         </noscript>
       </body>
