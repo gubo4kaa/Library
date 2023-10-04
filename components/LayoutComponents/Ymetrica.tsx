a36355eba84809042c697e0c42c29db1ab3c7ae1
Danila <dany.den2014@yandex.ru>
2023-10-04 14:13:46 +0300

    add useEffect in YM component

--- components/LayoutComponents/Ymetrica.tsx
+++ components/LayoutComponents/Ymetrica.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { DetailedHTMLProps, HTMLAttributes } from 'react'
+import { DetailedHTMLProps, HTMLAttributes, useEffect } from 'react'
 import styles from './ym.module.css'
 import ym, { YMInitializer } from 'react-yandex-metrika';
 import { Router } from 'next/router';
@@ -13,32 +13,16 @@ interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,HTMLDiv
 }
 
 export default function Ymetrica({}:Props) {
-  // Router.events.on('routeChangeComplete',(url: string) => {
-  //   if(typeof window !== undefined) {
-  //     ym('hit', url)
-  //   }
-  // })
+
+  useEffect(() => {
+    Router.events.on('routeChangeComplete',(url: string) => {
+      if(typeof window !== undefined) {
+        ym('hit', url)
+      }
+    })
+  },[])
+
   return <div>
-    {/* {
-      parse(
-        `
-          <script type="text/javascript" >
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-      
-          ym(95109351, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-          });
-          </script>
-      `
-      )
-    } */}
     <Analytics/>
     <YMInitializer
       accounts={[95109351]}
