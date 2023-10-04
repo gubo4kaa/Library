e92823fa65815d4bdff6b26ae0468e5aef01879a
Danila <dany.den2014@yandex.ru>
2023-10-04 15:01:28 +0300

    debug YM

--- components/LayoutComponents/Ymetrica.tsx
+++ components/LayoutComponents/Ymetrica.tsx
@@ -38,7 +38,6 @@ export default function Ymetrica({}:Props) {
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-      
           ym(95109351, "init", {
               clickmap:true,
               trackLinks:true,
