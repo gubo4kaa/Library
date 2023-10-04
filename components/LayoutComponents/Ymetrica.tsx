e41f04a8f1d6ec397646fc5e82c3dee439888e81
Danila <dany.den2014@yandex.ru>
2023-10-04 14:11:14 +0300

    Delete parse YM

--- components/LayoutComponents/Ymetrica.tsx
+++ components/LayoutComponents/Ymetrica.tsx
@@ -19,30 +19,32 @@ export default function Ymetrica({}:Props) {
   //   }
   // })
   return <div>
-    {
-      parse(`
-      <script type="text/javascript" >
-      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      m[i].l=1*new Date();
-      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-   
-      ym(95109351, "init", {
-           clickmap:true,
-           trackLinks:true,
-           accurateTrackBounce:true,
-           webvisor:true
-      });
-   </script>
-      `)
-    }
-    {/* <Analytics/>
+    {/* {
+      parse(
+        `
+          <script type="text/javascript" >
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+          ym(95109351, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+          });
+          </script>
+      `
+      )
+    } */}
+    <Analytics/>
     <YMInitializer
       accounts={[95109351]}
       options={{webvisor: true,defer: true}}
       version='2'
-    /> */}
+    />
     </div>
 }
 
