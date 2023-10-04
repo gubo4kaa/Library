d6d912f0fb83c1c70cbee32c4b78ec5035212062
Danila <dany.den2014@yandex.ru>
2023-10-04 14:29:00 +0300

    add parse function

--- components/LayoutComponents/Ymetrica.tsx
+++ components/LayoutComponents/Ymetrica.tsx
@@ -4,6 +4,7 @@ import styles from './ym.module.css'
 import ym, { YMInitializer } from 'react-yandex-metrika';
 import { Router } from 'next/router';
 import { Analytics } from '@vercel/analytics/react';
+import parse from 'html-react-parser';
 
 
 
@@ -22,12 +23,32 @@ export default function Ymetrica({}:Props) {
   // },[])
 
   return <div>
-    <Analytics/>
+    {
+      parse(
+        `
+          <script type="text/javascript" >
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+          ym(95109351, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+          });
+          </script>
+      `
+      )
+    }
+    {/* <Analytics/>
     <YMInitializer
       accounts={[95109351]}
       options={{webvisor: true,defer: true}}
       version='2'
-    />
+    /> */}
     </div>
 }
 
