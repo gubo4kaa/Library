8804d2a6d64fa6c0b4dbaed7182e9fde179a0e66
Danila <dany.den2014@yandex.ru>
2023-10-04 14:20:16 +0300

    debug yandex metrica

--- components/LayoutComponents/Ymetrica.tsx
+++ components/LayoutComponents/Ymetrica.tsx
@@ -4,7 +4,6 @@ import styles from './ym.module.css'
 import ym, { YMInitializer } from 'react-yandex-metrika';
 import { Router } from 'next/router';
 import { Analytics } from '@vercel/analytics/react';
-import parse from 'html-react-parser';
 
 
 
@@ -14,13 +13,13 @@ interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,HTMLDiv
 
 export default function Ymetrica({}:Props) {
 
-  useEffect(() => {
-    Router.events.on('routeChangeComplete',(url: string) => {
-      if(typeof window !== undefined) {
-        ym('hit', url)
-      }
-    })
-  },[])
+  // useEffect(() => {
+  //   Router.events.on('routeChangeComplete',(url: string) => {
+  //     if(typeof window !== undefined) {
+  //       ym('hit', url)
+  //     }
+  //   })
+  // },[])
 
   return <div>
     <Analytics/>
