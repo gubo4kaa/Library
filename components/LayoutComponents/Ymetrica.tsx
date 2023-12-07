298522b839a7f2f24251f2d25a967ca50327de5b
Danila <dany.den2014@yandex.ru>
2023-12-07 13:29:24 +0300

    on vebvisor

--- components/LayoutComponents/Ymetrica.tsx
+++ components/LayoutComponents/Ymetrica.tsx
@@ -11,15 +11,6 @@ interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,HTMLDiv
 }
 
 export default function Ymetrica({}:Props) {
-
-  // useEffect(() => {
-  //   Router.events.on('routeChangeComplete',(url: string) => {
-  //     if(typeof window !== undefined) {
-  //       ym('hit', url)
-  //     }
-  //   })
-  // },[])
-
   Router.events.on('routeChangeComplete',(url: string) => {
     if(typeof window !== undefined) {
       ym('hit', url)
@@ -27,32 +18,7 @@ export default function Ymetrica({}:Props) {
   })
 
   return <div>
-    {/* {
-      parse(
-        `
-          <script type="text/javascript">
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-          ym(95109351, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-          });
-          </script>
-      `
-      )
-    }
-     */}
-    {/* <YMInitializer
-      accounts={[95109351]}
-      options={{webvisor: true,defer: true}}
-      version='2'
-    /> */}
-      <YMInitializer accounts={[95789485]} />
+      <YMInitializer accounts={[95789485]} options={{webvisor: true}} />
     </div>
 }
 
