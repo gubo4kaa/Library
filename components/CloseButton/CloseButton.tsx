2bbe7c12c3855dfb39016f11030f6964d9098bf2
Danila <dany.den2014@yandex.ru>
2023-12-09 10:44:51 +0300

    debug svg

--- components/CloseButton/CloseButton.tsx
+++ components/CloseButton/CloseButton.tsx
@@ -1,12 +1,9 @@
 "use client"
-import { DetailedHTMLProps, HTMLAttributes } from 'react'
-import styles from './CloseButton.module.css'
-import BurgerLogo from './Burger.svg'
-import Image from "next/image";
-import CloseLogo from './Close.svg'
+import LibraryService from '@/services/services';
 import { useOpenMenuStore } from '@/store/store';
 import { motion } from 'framer-motion';
-import LibraryService from '@/services/services';
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import styles from './CloseButton.module.css';
 
 
 interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,HTMLDivElement> {
@@ -31,7 +28,7 @@ export default function CloseButton({}:Props) {
     className={styles.wrapper} onClick={() => {setOpenMenu(false); searchFunction('ui')}}>
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g id="Close">
-      <path id="Vector" d="M5.83325 5.83301L9.99992 9.99967M9.99992 9.99967L5.83325 14.1663M9.99992 9.99967L14.1666 5.83301M9.99992 9.99967L14.1666 14.1663" stroke="#6E7A90" stroke-width="1.92" stroke-linecap="round" stroke-linejoin="round"/>
+      <path id="Vector" d="M5.83325 5.83301L9.99992 9.99967M9.99992 9.99967L5.83325 14.1663M9.99992 9.99967L14.1666 5.83301M9.99992 9.99967L14.1666 14.1663" stroke="#6E7A90" strokeWidth="1.92" strokeLinecap="round" strokeLinejoin="round"/>
       </g>
     </svg>
   </motion.div>
