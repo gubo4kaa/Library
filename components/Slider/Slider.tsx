1b09c28d9e993ab4ab69e0faef9aa8013c9da992
Aidar Nigmatullin <gudwinthegreat@gmail.com>
2024-02-09 17:59:31 +0300

    debug

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -9,6 +9,9 @@ import MainCard from '../MainCard/MainCard';
 import styles from './Slider.module.css';
 import cn from 'classnames';
 import { SwiperButtonNext } from './sliderComponent/sliderButton/SliderButton';
+import swiper from 'swiper';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+
 
 interface ISlider {
 	categories: ICategory[];
@@ -16,11 +19,7 @@ interface ISlider {
 }
 
 export default function Slider({categories, services}: ISlider) {
-	const [activeIndex, setActiveIndex] = useState(0);
-
-  const handleClick = (key: number) => {
-    setActiveIndex(key);
-  };
+	
 
   return (
     <div className={styles.wrapper}>
@@ -30,20 +29,21 @@ export default function Slider({categories, services}: ISlider) {
       <Swiper
         spaceBetween={16}
         slidesPerView={3}
+        onSlideChange={() => {console.log('Ghbdtn')}}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
       >
         {services.map((service, key) => (
           <SwiperSlide key={key}>
 				    <MainCard service={service} categories={categories}/>
           </SwiperSlide>
         ))}
-      </Swiper>
       <div className={styles.wrapperPagination}>  
         <div className={styles.wrapperTabs}>
           {services.map((service, key) => (
             <span
               key={key}
               className={cn(styles.linkToSlide, {
-                [styles.activeLink]: activeIndex == key
+                [styles.activeLink]: key == 0
               })}
             >
               {
@@ -53,10 +53,23 @@ export default function Slider({categories, services}: ISlider) {
           ))}
         </div>
         <div className={styles.wrapperArrows}>
-              
-            asdf
+            <span>
+              <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="Arrow Left">
+                  <path id="Vector 190" d="M3.66658 10.9997L9.85409 17.4163M3.66658 10.9997L9.85409 4.58301M3.66658 10.9997L18.3333 10.9997" stroke="#6E7A90" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
+                </g>
+              </svg>
+            </span>
+            <span onClick={() => swiper.slideNext()}>
+              <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="Arrow Right">
+                  <path id="Vector 190" d="M18.3334 10.9997L12.1459 17.4163M18.3334 10.9997L12.1459 4.58301M18.3334 10.9997L3.66675 10.9997" stroke="#6E7A90" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
+                </g>
+              </svg>
+            </span>
         </div>
       </div>
+      </Swiper>
     </div>
   );
 }
