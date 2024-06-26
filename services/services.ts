c271daf57332fb8c77f8981d8c17f91fc105d6e8
Danil <danilfalka@gmail.com>
2024-06-26 23:43:25 +0300

    debag ls send email

--- services/services.ts
+++ services/services.ts
@@ -1,41 +1,6 @@
 import { $api, $apiLemonsqueezy } from "@/http/axios";
 import { AxiosResponse } from "axios";
 
-const configLemonsqueezy = {
-    headers:{
-        'Accept': 'application/vnd.api+json',
-        'Content-Type': 'application/vnd.api+json',
-        'Access-Control-Allow-Origin': "*",
-        'Authorization': `Bearer ${process.env.NEXT_PUBLIC_LEMONSQUEEZY_KEY}`
-    }
-  };
-
-const testSubscriber = {
-    "type": "customers",
-    "id": "1",
-    "attributes": {
-      "store_id": 1,
-      "name": "Luke Skywalker",
-      "email": "lukeskywalker@example.com",
-      "status": "subscribed",
-      "city": null,
-      "region": null,
-      "country": "US",
-      "total_revenue_currency": 84332,
-      "mrr": 1999,
-      "status_formatted": "Subscribed",
-      "country_formatted": "United States",
-      "total_revenue_currency_formatted": "$843.32",
-      "mrr_formatted": "$19.99",
-      "urls": {
-        "customer_portal": "https://uiscore.lemonsqueezy.com/billing?expires=1666869343&signature=82ae290ceac8edd4190c82825dd73a8743346d894a8ddbc4898b97eb96d105a5"
-      },
-      "created_at": "2022-12-01T13:01:07.000000Z",
-      "updated_at": "2022-12-09T09:05:21.000000Z",
-      "test_mode": false
-    }
-  }
-
 export default class LibraryService {
     static async Search(searchString: string): Promise<AxiosResponse<IServiceInterface[]>> {
         return $api.get<IServiceInterface[]>(`library/search-item/?searchString=${searchString}`)
@@ -56,30 +21,7 @@ export default class LibraryService {
     static async EmailService(body: any): Promise<AxiosResponse<IServiceInterface[]>> {
         //console.log(body);
         //console.log(body.email);
-        const formUrl = 'https://uiscore.lemonsqueezy.com/email-subscribe/external'
-        const redirectUrl = 'https://dev.uiscore.library.io/done/'
-        const value = new FormData();
-        value.append('email', body.email);
-        try {
-            await fetch(formUrl, {
-                method: 'POST',
-                body: value,
-            })
-            .then((data) => {
-                //console.log(data);
-                //console.log('access!!!');
-            })
-            //console.log('in tryCatch')
-          } catch (error) {
-            alert('Sorry, there was an issue: ' + error);
-          }
         return await $api.post<IServiceInterface[]>(`library/save-new-mail`, body)
-    }
-
-    static async EmailServiceLemonsqueezy(body: object): Promise<AxiosResponse<IServiceInterface[]>> {
-        //console.log(configLemonsqueezy);
-        //console.log(testSubscriber);
-        return $apiLemonsqueezy.post<any>(`/`, testSubscriber)
     }
 
     static async Report(body: any): Promise<AxiosResponse<object>> {
