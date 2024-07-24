111dd3719f26bd6139ed41f5d3eff3177dcf6faa
Danil <danilfalka@gmail.com>
2024-07-24 10:35:57 +0300

    lazy loading image

--- components/MainCard/MainCard.tsx
+++ components/MainCard/MainCard.tsx
@@ -69,6 +69,7 @@ export default function MainCard({categories, service}:Props) {
                             height: 'auto',
                         }}
                         // priority={true}
+                        loading='lazy'
                         className={styles.img}/>
                     <svg className={styles.arrow} width="22" height="23" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g id="Arrow Right">
