35563ff2b933f649d7c7e9a38a0588716a7c66c9
Danil <danilfalka@gmail.com>
2024-10-03 12:34:01 +0300

    add new bunner

--- components/Bunner/Bunner.tsx
+++ components/Bunner/Bunner.tsx
@@ -1,29 +1,31 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react'
-import styles from './Bunner.module.css'
-import cn from 'classnames'
-import ButtonNew from '../ButtonNew/ButtonNew'
-import Image from 'next/image'
-import ButtonBunner from './buttonBunner/buttonBunner'
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+import styles from "./Bunner.module.css";
+import cn from "classnames";
+import ButtonNew from "../ButtonNew/ButtonNew";
+import Image from "next/image";
+import ButtonBunner from "./buttonBunner/buttonBunner";
+import Link from "next/link";
 
-interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,HTMLDivElement> {
-   
+interface Props
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
+
+export default function Bunner({ className }: Props) {
+  return (
+    <div className={cn(styles.wrapper, className)}>
+      <img className={styles.img__mb} src="/bunner/bilbord-mb3.png" alt="" />
+      <div className={cn(styles.main)}>
+        <h1 className="">Download Free iPhone 16 Pro Mockup</h1>
+        <p>Actual device mockups with excellent quality for free!</p>
+        {/* <ButtonBunner/> */}
+        <Link href={"https://www.figma.com/community/file/1417516691208022438/iphone-16-pro-free-mockups"}>
+          <ButtonNew
+            size="es"
+          >
+            Download for Free
+          </ButtonNew>
+        </Link>
+      </div>
+      <img className={styles.img} src="/bunner/bilbord.png" alt="" />
+    </div>
+  );
 }
-
-export default function Bunner({className}:Props) {
-    return (
-        <div className={cn(styles.wrapper, className)}>
-            <div className={cn(styles.main)}>
-                <h1>
-                    Ultimate collection for creativity & productivity
-                </h1>
-                <p>
-                    Browse the best resources with weekly updates!
-                </p>
-                <ButtonBunner/>
-            </div>
-            <img className={styles.img} src='/bunner/bunner-bg.png' alt="" />
-            <img className={styles.img__mb} src='/bunner/bunner-bg__mobile.png' alt="" />
-        </div>
-    )
-}
-
--- components/LayoutComponents/Sidebar/Sidebar.tsx
+++ components/LayoutComponents/Sidebar/Sidebar.tsx
@@ -1,27 +1,35 @@
-import ButtonNew from '@/components/ButtonNew/ButtonNew';
-import cn from 'classnames';
+import ButtonNew from "@/components/ButtonNew/ButtonNew";
+import cn from "classnames";
 import Image from "next/image";
-import Link from 'next/link';
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-import CategoryList from './CategoryList/CategoryList';
-import styles from './Sidebar.module.css';
+import Link from "next/link";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+import CategoryList from "./CategoryList/CategoryList";
+import styles from "./Sidebar.module.css";
 
-interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,HTMLDivElement> {
-  categories: ICategory[]
+interface Props
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  categories: ICategory[];
 }
 // encodeURIComponent(category.nameCategory.trim())
 
-const Sidebar = ({categories, className}: Props) => {
-      return (
-      <div className={cn(styles.wrapper, className)}>
-        <div className={styles.stickyWrapper}>
-          <div className={styles.scrollWrapper}> 
-            <Link href={'/'} className={styles.linkLogo}> 
-              <Image src={'/LogoLibrary3.svg'} priority={true} quality={100} width={152} height={40} alt={''}/>
-            </Link>
-            <p className={styles.subtitle}>Categories</p>
-            <CategoryList category={categories}/>
-            {/* <Link href={`${process.env.NEXT_PUBLIC_UISCORE}`} className={styles.cardLink}>
+const Sidebar = ({ categories, className }: Props) => {
+  return (
+    <div className={cn(styles.wrapper, className)}>
+      <div className={styles.stickyWrapper}>
+        <div className={styles.scrollWrapper}>
+          <Link href={"/"} className={styles.linkLogo}>
+            <Image
+              src={"/LogoLibrary3.svg"}
+              priority={true}
+              quality={100}
+              width={152}
+              height={40}
+              alt={""}
+            />
+          </Link>
+          <p className={styles.subtitle}>Categories</p>
+          <CategoryList category={categories} />
+          {/* <Link href={`${process.env.NEXT_PUBLIC_UISCORE}`} className={styles.cardLink}>
               <div className={styles.UiscoreLogo}>
                 <video width="218" loop autoPlay muted height="120">
                   <source src="./video/BannerSite.mp4" type="video/mp4"/>
@@ -38,48 +46,105 @@ const Sidebar = ({categories, className}: Props) => {
                 </p>
               </div>
             </Link> */}
-            <div className={cn(styles.UiscoreLogo, styles.cardHelp)}>
-              <h5>
-                Help us become better! 
-              </h5>
-              <p>
-                Take a short survey on what could be improved in the library.
-              </p>
-              <ButtonNew href='https://forms.gle/Vu6PfB9XThuwcX7NA' size='es' iconPosition={'iconRight'} width='max'>Take the survey <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
-                  <path d="M17.1666 10.3792L11.5416 16.2126M17.1666 10.3792L11.5416 4.5459M17.1666 10.3792L3.83325 10.3792" stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-              </ButtonNew>
-            </div>
-            <div className={styles.tuk}></div>
-            <ButtonNew href='https://uiscore.lemonsqueezy.com/buy/101c4821-3cd0-4787-900c-fad723198647?embed=1&logo=0&discount=0' type='Default Ghost' className={"lemonsqueezy-button"} size='es' iconPosition={'iconRight'} width='max'>
-              Support Us
+          <div className={cn(styles.UiscoreLogo, styles.cardHelp)}>
+            <Image
+              src={"/sidebaricons.png"}
+              height={231}
+              width={339}
+              alt={""}
+              className={styles.logoes}
+            ></Image>
+            <h5>Free design assets</h5>
+            <p>Follow us on figma to get free design assets</p>
+            <ButtonNew
+              href="https://www.figma.com/@uiscore"
+              size="es"
+              iconPosition={"iconRight"}
+              width="max"
+            >
+              Figma Community{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="21"
+                height="21"
+                viewBox="0 0 21 21"
+                fill="none"
+              >
+                <path
+                  d="M17.1666 10.3792L11.5416 16.2126M17.1666 10.3792L11.5416 4.5459M17.1666 10.3792L3.83325 10.3792"
+                  stroke="white"
+                  stroke-width="1.6"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
             </ButtonNew>
-            <footer className={styles.footer}>
-              <div className={styles.linkWrapper}>
-                <Link href={'https://dribbble.com/uiscore'} className={styles.link}>
-                  <Image src={'/icons/Dribbble.svg'} height={20} width={20}  alt={''}></Image>
-                </Link>
-                <Link href={'https://twitter.com/uiscore'} className={styles.link}>
-                  <Image src={'/icons/Twitter.svg'} height={20} width={20}  alt={''}></Image>
-                </Link>
-                <Link href={'https://www.instagram.com/uiscore.io'} className={styles.link}>
-                  <Image src={'/icons/Instagram.svg'} height={20} width={20}  alt={''}></Image>
-                </Link>
-                <Link href={'https://layers.to/uiscore'} className={styles.link}>
-                  <Image src={'/icons/Layers.svg'} height={20} width={20}  alt={''}></Image>
-                </Link>
-              </div>
-              <p>
-                © 2024 Made by Uiscore<br/>
-                <a href="mailto:libary@uiscore.io">
-                  libary@uiscore.io
-                </a>
-              </p>
-            </footer>
           </div>
+          <div className={styles.tuk}></div>
+          <ButtonNew
+            href="https://uiscore.lemonsqueezy.com/buy/101c4821-3cd0-4787-900c-fad723198647?embed=1&logo=0&discount=0"
+            type="Default Ghost"
+            className={"lemonsqueezy-button"}
+            size="es"
+            iconPosition={"iconRight"}
+            width="max"
+          >
+            Support Us
+          </ButtonNew>
+          <footer className={styles.footer}>
+            <div className={styles.linkWrapper}>
+              <Link
+                href={"https://dribbble.com/uiscore"}
+                className={styles.link}
+              >
+                <Image
+                  src={"/icons/Dribbble.svg"}
+                  height={20}
+                  width={20}
+                  alt={""}
+                ></Image>
+              </Link>
+              <Link
+                href={"https://twitter.com/uiscore"}
+                className={styles.link}
+              >
+                <Image
+                  src={"/icons/Twitter.svg"}
+                  height={20}
+                  width={20}
+                  alt={""}
+                ></Image>
+              </Link>
+              <Link
+                href={"https://www.instagram.com/uiscore.io"}
+                className={styles.link}
+              >
+                <Image
+                  src={"/icons/Instagram.svg"}
+                  height={20}
+                  width={20}
+                  alt={""}
+                ></Image>
+              </Link>
+              <Link href={"https://layers.to/uiscore"} className={styles.link}>
+                <Image
+                  src={"/icons/Layers.svg"}
+                  height={20}
+                  width={20}
+                  alt={""}
+                ></Image>
+              </Link>
+            </div>
+            <p>
+              © 2024 Made by Uiscore
+              <br />
+              <a href="mailto:libary@uiscore.io">libary@uiscore.io</a>
+            </p>
+          </footer>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
